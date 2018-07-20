c588c88ca538ba3a91b9b3ea2a5fc9b55a479d42
Soorya Narayan <soorya.narayan.jm@gmail.com>
2018-07-20 17:29:24 +0530

    test exit travis

--- scripts/run-lh.sh
+++ scripts/run-lh.sh
@@ -2,17 +2,6 @@
 
 LIQUID="$HOME/liquidhaskell/.cabal-sandbox/bin/liquid"
 $LIQUID --version
-echo "-------------------------------------------------"
-pwd
-echo "-------------------------------------------------"
-cd ./raaz-core
-pwd
-echo "-------------------------------------------------"
-find . -name '*.hs' -print0 | xargs -n 1 -0 -I{} sh -c "echo ---- {}; $LIQUID {}"
-cd ..
-pwd
-echo "-------------------------------------------------"
-cd raaz-implementation
-pwd
-echo "-------------------------------------------------"
-find . -name '*.hs' -print0 | xargs -n 1 -0 -I{} sh -c "echo ---- {}; $LIQUID {}"
+
+$LIQUID $(find raaz-core raaz-implementation -name '*.hs' ! -name "Entropy.hs" | paste -sd " ")
+exit $?
